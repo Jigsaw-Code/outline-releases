b1d5b8f67a096ff38697e953595b9b833fa679ef
Jon Cohen <cohenjon@google.com>
2019-06-24 18:28:24 -0400

    Upload to the S3 bucket on release.  This commit has corresponding changes:
    in outline-client to pull from the s3 bucket with electron-update
    
    and in the permissions to the s3 bucket, which should be inspected
    separately before approving this PR

--- release_linux_client.sh
+++ release_linux_client.sh
@@ -18,6 +18,15 @@
 # prepares a commit on a new branch from which a pull request can easily
 # be made.
 
+if [[ ! -f ~/.aws/credentials ]]; then
+  echo "No AWS credentials file found.  Follow the instructions in the README to create one"
+  exit 1
+fi
+if [[ ! grep --quiet '\[outline-releases\]' ~/.aws/credentials ]]; then
+  echo "No outline-releases profile found in AWS credentuals"
+  exit 1
+fi
+
 declare -a FILES=(
   Outline-Client.AppImage
   latest-linux.yml
@@ -84,3 +93,4 @@ git checkout -b linux-client-$VERSION
 git commit -a -m "release linux client $VERSION"
 git branch
 git push origin linux-client-$VERSION
+aws s3 sync client s3://outline-releases/client --profile=outline-releases
